c9118a2eeb82e8d7ead659fd60c81dd620dbfe9a
mengbw <mengbw@asiainfo.com>
2026-07-03 11:32:23 +0800

    四  - AI-生成应用后端
    1.实现AI应用生成（远生模式：接入模型、开发AI服务、结构化输出）
    2.实现流式输出的模式：LangChain4j + Reactor（实现方案）

--- yu-ai-code-mother-frontend/src/App.vue
+++ yu-ai-code-mother-frontend/src/App.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
-import GlobalHeader from '@/components/GlobalHeader.vue'
-import GlobalFooter from '@/components/GlobalFooter.vue'
-import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <a-layout>
-    <GlobalHeader />
-    <a-layout-content>
-      <RouterView />
-    </a-layout-content>
-    <GlobalFooter />
-  </a-layout>
+  <BasicLayout />
 </template>
