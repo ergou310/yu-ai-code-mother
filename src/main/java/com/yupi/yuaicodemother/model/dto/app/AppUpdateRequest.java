e517f3f21f9a1c63f15c8891061d6151ef05228d
mengbw <mengbw@asiainfo.com>
2026-07-03 15:32:17 +0800

    五  - 应用模块（完成基本功能）
    1. AI 生成基础功能模块
    2. 完成基本功能、人工测试通过

---
+++ src/main/java/com/yupi/yuaicodemother/model/dto/app/AppUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.yupi.yuaicodemother.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 更新用户求情
+ */
+@Data
+public class AppUpdateRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 应用名称
+     */
+    private String appName;
+
+    private static final long serialVersionUID = 1L;
+}
